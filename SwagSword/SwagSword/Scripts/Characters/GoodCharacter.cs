4e62414f3a92e98ce058c309573c4f6d9aa6a6c2
Nelson Scott <officialbadbeargames@gmail.com>
2015-03-04 14:05:49 -0500

    Character Class
    Added a physics update and Faction enum

---
+++ SwagSword/SwagSword/Scripts/Characters/GoodCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SwagSword
+{
+    class GoodCharacter : Character
+    {
+        public GoodCharacter(int x, int y, Texture2D texture): base(x, y, texture)
+        {
+
+        }
+    }
+}

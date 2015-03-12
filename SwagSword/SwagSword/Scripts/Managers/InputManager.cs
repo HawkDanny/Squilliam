1a6fdf689c33fa061c3b16db790e971b27046f10
Danny Hawk <dhawk@rochester.rr.com>
2015-03-11 21:16:39 -0400

    Whoops.
    These should've been included, they weren't.

--- SwagSword/SwagSword/Scripts/Managers/InputManager.cs
+++ SwagSword/SwagSword/Scripts/Managers/InputManager.cs
@@ -7,83 +7,57 @@ using Microsoft.Xna.Framework.Input;
 
 namespace SwagSword
 {
-    /// <summary>
-    /// Input Manager figures out the input, but shouldn't change anything using it
-    /// </summary>
- 
+    //Enum necessary for specifying mouse buttons in the inputMouse class   
+    public enum MouseButton
+    {
+        Left, Right, Middle
+    }
+
+    //Enum containg all the different actions bindable to keys/buttons
+    public enum Binds
+    {
+        Up, Down, Left, Right, Attack, UseAbility, Ability1, Ability2, Ability3, Ability4
+    }
 
     public class InputManager:Manager
     {
         //Fields
-        KeyboardState prevKbState;
-        KeyboardState kbState;
-        MouseState mState;
-        Keys up, down, left, right, attack, useAbility, cycleAbilitiesUp, cycleAbilitiesDown;
-        int mouseX, mouseY;
+        private KeyboardState prevKbState; //previous keyboard state
+        private KeyboardState kbState;
+        private MouseState prevMState; //previous mouse state
+        private MouseState mState;
+        
+        //list of all bound keys/buttons, in order of enum Binds (Line 19)
+        private InputType[] binds;
 
-        //Gets and Sets for all properties
-        public Keys Up
-        {
-            get { return up; }
-            set { up = value; }
-        }
-        public Keys Down
-        {
-            get { return down; }
-            set { down = value; }
-        }
-        public Keys Left
-        {
-            get { return left; }
-            set { left = value; }
-        }
-        public Keys Right
-        {
-            get { return right; }
-            set { right = value; }
-        }
-        public Keys Attack
-        {
-            get { return attack; }
-            set { attack = value; }
-        }
-        public Keys UseAbility
-        {
-            get { return useAbility; }
-            set { useAbility = value; }
-        }
-        public Keys CycleAbilitiesUp
-        {
-            get { return cycleAbilitiesUp; }
-            set { cycleAbilitiesUp = value; }
-        }
-        public Keys CycleAbilitiesDown
-        {
-            get { return cycleAbilitiesDown; }
-            set { cycleAbilitiesDown = value; }
-        }
-        public int MouseX
-        {
-            get { return mouseX; }
-            set { mouseX = value; }
-        }
-        public int MouseY
-        {
-            get { return mouseY; }
-            set { mouseY = value; }
-        }
+        //Get properties for all actions
+        public InputType Up { get { return binds[0]; } }
+        public InputType Down { get { return binds[1]; } }
+        public InputType Left { get { return binds[2]; } }
+        public InputType Right { get { return binds[3]; } }
+        public InputType Attack { get { return binds[4]; } }
+        public InputType UseAbility { get { return binds[5]; } }
+        public InputType Ability1 { get { return binds[6]; } }
+        public InputType Ability2 { get { return binds[7]; } }
+        public InputType Ability3 { get { return binds[8]; } }
+        public InputType Ability4 { get { return binds[9]; } }
+
+        
 
         public InputManager(Game1 mainMan):base(mainMan)
         {
 
         }
 
-        //Init
+        //Initialize
         public override void Init()
         {
             kbState = new KeyboardState();
             prevKbState = new KeyboardState();
             mState = new MouseState();
+
+            binds = new InputType[10];
+
             Default();
         }
 
@@ -97,36 +71,282 @@ namespace SwagSword
             mState = Mouse.GetState();
         }
 
+        /// <summary>
+        /// Sets the default key bindings
+        /// </summary>
         public void Default()
         {
-            up = Keys.W;
-            left = Keys.A;
-            right = Keys.D;
-            down = Keys.S;
-
-            attack = Keys.Left;
-            useAbility = Keys.Right;
-            cycleAbilitiesUp = Keys.Q;
-            cycleAbilitiesDown = Keys.E;
-
-            mouseX = mState.X;
-            mouseY = mState.Y;
+            binds[0] = new InputMouse(mState, MouseButton.Left);
+            binds[1] = new InputKeyboard(kbState, Keys.S);
+            binds[2] = new InputKeyboard(kbState, Keys.A);
+            binds[3] = new InputKeyboard(kbState, Keys.D);
+            binds[4] = new InputMouse(mState, MouseButton.Left);
+            binds[5] = new InputMouse(mState, MouseButton.Right);
+            binds[6] = new InputKeyboard(kbState, Keys.D1);
+            binds[7] = new InputKeyboard(kbState, Keys.D2);
+            binds[8] = new InputKeyboard(kbState, Keys.D3);
+            binds[9] = new InputKeyboard(kbState, Keys.D4);
         }
 
-        public bool IsKeyDown(Keys key)
+        public void Bind(Binds keyToBind)
         {
-            if (kbState.IsKeyDown(key))
-                return true;
-            else
-                return false;
-        }
+            //Previous states of mouse buttons
+            ButtonState leftState, middleState, rightState;
+            leftState = prevMState.LeftButton;
+            middleState = prevMState.MiddleButton;
+            rightState = prevMState.RightButton;
 
-        public bool IsKeyUp(Keys key)
-        {
-            if (kbState.IsKeyUp(key))
-                return true;
-            else
-                return false;
+            Keys[] prevPressedKeys = prevKbState.GetPressedKeys(); //list of the key presses in the previous keyboard state
+            Keys[] pressedKeys = kbState.GetPressedKeys(); //list of the key presses in the current keyboard state
+
+            /*
+             * Each case is for a different action
+             * Creates an inputType object that's either mouse or keyboard depending on the input
+             * Stores the inputType object in binds in its corresponding index
+             * */
+            switch (keyToBind)
+            {
+                case Binds.Up:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard up = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[0] = up;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse up = new InputMouse(mState, MouseButton.Left);
+                        binds[0] = up;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse up = new InputMouse(mState, MouseButton.Middle);
+                        binds[0] = up;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse up = new InputMouse(mState, MouseButton.Right);
+                        binds[0] = up;
+                    }
+                    else
+                        break;
+                    break;
+                case Binds.Down:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard down = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[1] = down;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse down = new InputMouse(mState, MouseButton.Left);
+                        binds[1] = down;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse down = new InputMouse(mState, MouseButton.Middle);
+                        binds[1] = down;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse down = new InputMouse(mState, MouseButton.Right);
+                        binds[1] = down;
+                    }
+                    else
+                        break;
+                    break;
+                case Binds.Left:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard left = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[2] = left;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse left = new InputMouse(mState, MouseButton.Left);
+                        binds[2] = left;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse left = new InputMouse(mState, MouseButton.Middle);
+                        binds[2] = left;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse left = new InputMouse(mState, MouseButton.Right);
+                        binds[2] = left;
+                    }
+                    else
+                        break;
+                    break;
+                case Binds.Right:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard right = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[3] = right;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse right = new InputMouse(mState, MouseButton.Left);
+                        binds[3] = right;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse right = new InputMouse(mState, MouseButton.Middle);
+                        binds[3] = right;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse right = new InputMouse(mState, MouseButton.Right);
+                        binds[3] = right;
+                    }
+                    else
+                        break;
+                    break;
+                case Binds.Attack:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard attack = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[4] = attack;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse attack = new InputMouse(mState, MouseButton.Left);
+                        binds[4] = attack;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse attack = new InputMouse(mState, MouseButton.Middle);
+                        binds[4] = attack;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse attack = new InputMouse(mState, MouseButton.Right);
+                        binds[4] = attack;
+                    }
+                    else
+                        break;
+                    break;
+                case Binds.UseAbility:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard useAbility = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[5] = useAbility;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse useAbility = new InputMouse(mState, MouseButton.Left);
+                        binds[5] = useAbility;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse useAbility = new InputMouse(mState, MouseButton.Middle);
+                        binds[5] = useAbility;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse useAbility = new InputMouse(mState, MouseButton.Right);
+                        binds[5] = useAbility;
+                    }
+                    else
+                        break;
+                    break;
+                case Binds.Ability1:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard ability1 = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[6] = ability1;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse ability1 = new InputMouse(mState, MouseButton.Left);
+                        binds[6] = ability1;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse ability1 = new InputMouse(mState, MouseButton.Middle);
+                        binds[6] = ability1;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse ability1 = new InputMouse(mState, MouseButton.Right);
+                        binds[6] = ability1;
+                    }
+                    else
+                        break;
+                    break;
+                case Binds.Ability2:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard ability2 = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[7] = ability2;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse ability2 = new InputMouse(mState, MouseButton.Left);
+                        binds[7] = ability2;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse ability2 = new InputMouse(mState, MouseButton.Middle);
+                        binds[7] = ability2;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse ability2 = new InputMouse(mState, MouseButton.Right);
+                        binds[7] = ability2;
+                    }
+                    else
+                        break;
+                    break;
+                case Binds.Ability3:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard ability3 = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[8] = ability3;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse ability3 = new InputMouse(mState, MouseButton.Left);
+                        binds[8] = ability3;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse ability3 = new InputMouse(mState, MouseButton.Middle);
+                        binds[8] = ability3;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse ability3 = new InputMouse(mState, MouseButton.Right);
+                        binds[8] = ability3;
+                    }
+                    else
+                        break;
+                    break;
+                case Binds.Ability4:
+                    if (!(prevPressedKeys.Equals(pressedKeys)))
+                    {
+                        InputKeyboard ability4 = new InputKeyboard(kbState, pressedKeys[pressedKeys.Length - 1]);
+                        binds[9] = ability4;
+                    }
+                    else if (leftState != mState.LeftButton)
+                    {
+                        InputMouse ability4 = new InputMouse(mState, MouseButton.Left);
+                        binds[9] = ability4;
+                    }
+                    else if (middleState != mState.MiddleButton)
+                    {
+                        InputMouse ability4 = new InputMouse(mState, MouseButton.Middle);
+                        binds[9] = ability4;
+                    }
+                    else if (rightState != mState.RightButton)
+                    {
+                        InputMouse ability4 = new InputMouse(mState, MouseButton.Right);
+                        binds[9] = ability4;
+                    }
+                    else
+                        break;
+                    break;
+            }
         }
     }
 }

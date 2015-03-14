e052d38527f6b1496fb66372189da86f6ca0678b
pxl1778 <blackmateria55@aim.com>
2015-03-14 17:34:46 -0400

    GameOverScreen
    Didn't work the first time.

---
+++ SwagSword/SwagSword/Scripts/UIScreens/GameOverScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SwagSword
+{
+    class GameOverScreen : UIScreen
+    {
+        /// <summary>
+        /// Creates a screen that draws the game over image.
+        /// </summary>
+        /// <param name="mainMan"></param>
+        public GameOverScreen(Game1 mainMan):
+            base(mainMan)
+        {
+
+        }
+
+        public override void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(mainMan.DrawMan.GameOverImage, new Rectangle(0, 0, mainMan.WindowWidth, mainMan.WindowHeight), Color.White);
+        }
+    }
+}
--- SwagSword/SwagSword/Scripts/UIScreens/GameScreen.cs
+++ SwagSword/SwagSword/Scripts/UIScreens/GameScreen.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace SwagSword
 {
@@ -13,6 +14,11 @@ namespace SwagSword
         //Rectangle texture
         Texture2D rect;
 
+        /// <summary>
+        /// Creates the screen that holds the health bar and other
+        /// in game HUD.
+        /// </summary>
+        /// <param name="mainMan"></param>
         public GameScreen(Game1 mainMan):
             base(mainMan)
         {
@@ -22,7 +28,21 @@ namespace SwagSword
 
         public override void Update()
         {
-
+            //Checks to see if the characters health is done for.
+            //Obviously in game, this won't have to be a game over screen,
+            //but for checking that it works for now.
+            if(mainMan.GameMan.Characters[0].Health == 0)
+            {
+                mainMan.UIMan.Screens.Push(new GameOverScreen(mainMan));
+                mainMan.UIMan.State = GameState.gameOver;
+            }
+            
+            //Used to check how the health bar works.
+            //Press enter to decrease health by 1.
+            if(mainMan.InputMan.SingleKeyPress(Keys.Enter))
+            {
+                mainMan.GameMan.Characters[0].Health--;
+            }
         }
 
         public override void Draw(SpriteBatch spritebatch)
@@ -30,7 +50,9 @@ namespace SwagSword
             //Draws the back of the health bar as gray
             spritebatch.Draw(rect, new Rectangle(20, 20, 200, 10), Color.Gray);
             //Draws the actual Health meter as green
-            spritebatch.Draw(rect, new Rectangle(20, 20, (mainMan.GameMan.Characters[0].Health/mainMan.GameMan.Characters[0].MaxHealth)*200, 10), Color.Green);
+            double health = 0.0 + mainMan.GameMan.Characters[0].Health;
+            double maxHealth = 0.0 + mainMan.GameMan.Characters[0].MaxHealth;
+            spritebatch.Draw(rect, new Rectangle(20, 20, (int)((health/maxHealth)*200), 10), Color.Green);
             //Draws the number of health next to the bar
             spritebatch.DrawString(mainMan.DrawMan.HealthFont, mainMan.GameMan.Characters[0].Health + "/" + mainMan.GameMan.Characters[0].MaxHealth, new Vector2(225f, 14f), Color.Green);
             base.Draw(spritebatch);

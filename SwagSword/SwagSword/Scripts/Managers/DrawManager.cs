7ce99ec3861a25146ca38e771536f3c5fda9e991
RyanofRIT <42rbell@gmail.com>
2015-03-16 04:15:32 -0700

    Added Margins to Camera

--- SwagSword/SwagSword/Scripts/Managers/DrawManager.cs
+++ SwagSword/SwagSword/Scripts/Managers/DrawManager.cs
@@ -73,16 +73,16 @@ namespace SwagSword
             goodGuyTextures = new List<Texture2D>();
         }
 
-        public void Update()
+        public override void Update()
         {
             Vector2 movement = Vector2.Zero;
-            if (mainMan.InputMan.Left.IsDown())
+            if (mainMan.InputMan.Left.IsDown() && mainMan.GameMan.Players.ElementAt(0).X < camera.Position.X - mainMan.WindowWidth * 0.3)
                 movement.X--;
-            if (mainMan.InputMan.Right.IsDown())
+            if (mainMan.InputMan.Right.IsDown() && mainMan.GameMan.Players.ElementAt(0).X > camera.Position.X + mainMan.WindowWidth * 0.3)
                 movement.X++;
-            if (mainMan.InputMan.Up.IsDown())
+            if (mainMan.InputMan.Up.IsDown() && mainMan.GameMan.Players.ElementAt(0).Y < camera.Position.Y - mainMan.WindowHeight * 0.3)
                 movement.Y--;
-            if (mainMan.InputMan.Down.IsDown())
+            if (mainMan.InputMan.Down.IsDown() && mainMan.GameMan.Players.ElementAt(0).Y > camera.Position.Y + mainMan.WindowHeight * 0.3)
                 movement.Y++;
             camera.Position += movement * mainMan.GameMan.Characters.ElementAt(0).MovementSpeed;
         }

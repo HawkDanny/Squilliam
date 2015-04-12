2774cbd79ed444c713fefc3c7f8ec007a13bc97a
Nelson Scott <officialbadbeargames@gmail.com>
2015-04-12 15:10:00 -0400

    Added Ability Classes

---
+++ SwagSword/SwagSword/Scripts/Abilities/Ability.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+//Names: Nelson Scott
+
+namespace SwagSword
+{
+    class Ability
+    {
+        public Ability()
+        {
+
+        }
+
+        /// <summary>
+        /// Called when using the ability...
+        /// </summary>
+        public virtual void Use()
+        {
+
+        }
+    }
+}

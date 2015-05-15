2b30a115e8d70429ddb5e409da976c355c2f2ee1
RyanofRIT <rjb1928@NRH-1065-10.main.ad.rit.edu>
2015-05-15 04:57:42 -0400

    part 3

--- SwagSword/SwagSword/Scripts/Abilities/Decoy.cs
+++ SwagSword/SwagSword/Scripts/Abilities/Decoy.cs
@@ -65,9 +65,12 @@ namespace SwagSword
         /// </summary>
         public override void Use()
         {
+<<<<<<< HEAD
             position = new Vector2(character.X, character.Y);
             mainMan.SoundMan.Decoying.Play();
 
+=======
+>>>>>>> parent of e386464... Revert "stuff"
             position = new Vector2(character.X-32, character.Y-32);
 
             base.Use();
@@ -76,8 +79,11 @@ namespace SwagSword
         public override void AIUse()
         {
             position = new Vector2(character.X-32, character.Y-32);
+<<<<<<< HEAD
 
             mainMan.SoundMan.Decoying.Play();
+=======
+>>>>>>> parent of e386464... Revert "stuff"
             base.AIUse();
         }
 
